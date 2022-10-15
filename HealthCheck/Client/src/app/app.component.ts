474790817665809984f7b05a094c88d2fbbfd95e
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-15 16:06:55 -0700

    Get forecasts from our API on App component

--- HealthCheck/Client/src/app/app.component.ts
+++ HealthCheck/Client/src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { IWeatherForecast } from './shared/models/weatherForecast';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  baseApiUrl: string = environment.baseApiUrl;
+  weatherForecasts?: IWeatherForecast[];
   title = 'Client';
+
+  constructor(private http: HttpClient) {
+    this.http.get<IWeatherForecast[]>(this.baseApiUrl + 'weatherforecast')
+      .subscribe({
+        next: result => this.weatherForecasts = result,
+        error: error => console.log(error)
+      });
+  }
 }
