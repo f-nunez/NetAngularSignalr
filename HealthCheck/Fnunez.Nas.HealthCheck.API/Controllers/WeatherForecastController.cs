ed0b82ad7534f816ccb243634adc242272443570
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-15 16:03:33 -0700

    Add defaults api route on controller

--- HealthCheck/Fnunez.Nas.HealthCheck.API/Controllers/WeatherForecastController.cs
+++ HealthCheck/Fnunez.Nas.HealthCheck.API/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Fnunez.Nas.HealthCheck.API.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
