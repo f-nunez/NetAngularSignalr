8d5a98694fc749dfa845c004ba7e68b48462bfd8
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-15 16:04:26 -0700

    CORS Policy to allow our Angular client be comminated with API

--- HealthCheck/Fnunez.Nas.HealthCheck.API/Program.cs
+++ HealthCheck/Fnunez.Nas.HealthCheck.API/Program.cs
@@ -1,7 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+    });
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -17,6 +23,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
