d544c8e9358bd2429977b93ac4c5526e1412a203
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-18 16:25:18 -0700

    Add Material Paginator to module

--- WorldCities/Client/src/app/angular-material.module.ts
+++ WorldCities/Client/src/app/angular-material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -10,11 +11,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   imports: [
     MatButtonModule,
     MatIconModule,
+    MatPaginatorModule,
+    MatTableModule,
     MatToolbarModule
   ],
   exports: [
     MatButtonModule,
     MatIconModule,
+    MatPaginatorModule,
     MatTableModule,
     MatToolbarModule
   ]
