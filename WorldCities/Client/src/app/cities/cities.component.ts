9ddc1837865de3acd0b6507ddd9eaf3c747d41f5
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-18 16:47:40 -0700

    Add Material Paginator at component of Cities's table

--- WorldCities/Client/src/app/cities/cities.component.ts
+++ WorldCities/Client/src/app/cities/cities.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { environment } from 'src/environments/environment';
 import { ICity } from '../shared/models/city';
 
@@ -10,14 +12,18 @@ import { ICity } from '../shared/models/city';
 })
 export class CitiesComponent implements OnInit {
   baseApiUrl: string = environment.baseApiUrl;
-  cities!: ICity[];
+  cities!: MatTableDataSource<ICity>;
   displayedColumns: string[] = ['id', 'name', 'lat', 'lon'];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.http.get<ICity[]>(this.baseApiUrl + 'cities').subscribe({
-      next: response => this.cities = response,
+      next: response => {
+        this.cities = new MatTableDataSource<ICity>(response);
+        this.cities.paginator = this.paginator;
+      },
       error: error => console.log(error)
     })
   }
