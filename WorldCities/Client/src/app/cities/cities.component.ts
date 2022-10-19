62d2b57081e755e10f9e2e03e248cceb4c5bd2f0
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-18 17:28:27 -0700

    Implement ApiResult data fetched from Cities component

--- WorldCities/Client/src/app/cities/cities.component.ts
+++ WorldCities/Client/src/app/cities/cities.component.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { environment } from 'src/environments/environment';
 import { ICity } from '../shared/models/city';
@@ -19,13 +19,27 @@ export class CitiesComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get<ICity[]>(this.baseApiUrl + 'cities').subscribe({
-      next: response => {
-        this.cities = new MatTableDataSource<ICity>(response);
-        this.cities.paginator = this.paginator;
-      },
-      error: error => console.log(error)
-    })
+    const pageEvent: PageEvent = new PageEvent();
+    pageEvent.pageIndex = 0;
+    pageEvent.pageSize = 10;
+    this.getData(pageEvent);
+  }
+
+  getData(pageEvent: PageEvent) {
+    let params = new HttpParams()
+      .set('pageIndex', pageEvent.pageIndex.toString())
+      .set('pageSize', pageEvent.pageSize.toString());
+
+    this.http.get<any>(this.baseApiUrl + 'cities', { params })
+      .subscribe({
+        next: response => {
+          this.cities = new MatTableDataSource<ICity>(response.data);
+          this.paginator.length = response.totalCount;
+          this.paginator.pageIndex = response.pageIndex;
+          this.paginator.pageSize = response.pageSize;
+        },
+        error: error => console.log(error)
+      });
   }
 
 }
