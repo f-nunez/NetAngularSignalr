f40feaafc11f251357e32747728bd3c60b46a31c
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-20 17:26:53 -0700

    Remove unused import directives in CountriesComponent

--- WorldCities/Client/src/app/countries/countries.component.ts
+++ WorldCities/Client/src/app/countries/countries.component.ts
@@ -1,4 +1,3 @@
-import { HttpParams } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
