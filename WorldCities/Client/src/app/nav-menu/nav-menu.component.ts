cddf9e4b373a697a4d2462248209e2975d097955
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-22 16:19:18 -0700

    Use AuthService in NavMenuComponent

--- WorldCities/Client/src/app/nav-menu/nav-menu.component.ts
+++ WorldCities/Client/src/app/nav-menu/nav-menu.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { AuthService } from '../core/services/auth.service';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.scss']
 })
-export class NavMenuComponent implements OnInit {
+export class NavMenuComponent implements OnInit, OnDestroy {
+  private destroySubject = new Subject();
+  isLoggedIn: boolean = false;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.authService.authStatus
+      .pipe(takeUntil(this.destroySubject))
+      .subscribe({
+        next: response => {
+          this.isLoggedIn = response;
+        },
+        error: error => {
+          console.log(error);
+        }
+      });
+  }
 
   ngOnInit(): void {
+    this.isLoggedIn = this.authService.isAuthenticated();
+  }
+
+  ngOnDestroy(): void {
+    this.destroySubject.next(true);
+    this.destroySubject.complete();
+  }
+
+  onLogout(): void {
+    this.authService.logout();
+    this.router.navigate(['/']);
   }
 
 }
