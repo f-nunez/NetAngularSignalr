9dec4c545419448d013190eaa785492e0a2561d9
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-22 16:18:35 -0700

    Add logout and set auth status in our AuthService

--- WorldCities/Client/src/app/core/services/auth.service.ts
+++ WorldCities/Client/src/app/core/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject, tap } from 'rxjs';
 import { ILoginRequest } from 'src/app/shared/models/loginRequest';
 import { ILoginResult } from 'src/app/shared/models/loginResult';
 import { environment } from 'src/environments/environment';
@@ -9,13 +9,21 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class AuthService {
-  baseApiUrl: string = environment.baseApiUrl;
-  tokenKey: string = "token";
+  private baseApiUrl: string = environment.baseApiUrl;
+  private tokenKey: string = "token";
+  private _authStatus = new Subject<boolean>();
+  authStatus = this._authStatus.asObservable();
 
   constructor(protected http: HttpClient) { }
 
   getToken(): string | null {
     return localStorage.getItem(this.tokenKey);
+  }
+
+  init(): void {
+    if (this.isAuthenticated()) {
+      this.setAuthStatus(true);
+    }
   }
 
   isAuthenticated(): boolean {
@@ -24,7 +32,22 @@ export class AuthService {
 
   login(item: ILoginRequest): Observable<ILoginResult> {
     const url: string = this.baseApiUrl + 'account/login';
-    return this.http.post<ILoginResult>(url, item);
+    return this.http.post<ILoginResult>(url, item)
+      .pipe(tap(loginResult => {
+        if (loginResult.success && loginResult.token) {
+          localStorage.setItem(this.tokenKey, loginResult.token);
+          this.setAuthStatus(true);
+        }
+      }));
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.tokenKey);
+    this.setAuthStatus(false);
+  }
+
+  private setAuthStatus(isAuthenticated: boolean) {
+    this._authStatus.next(isAuthenticated);
   }
 
 }
