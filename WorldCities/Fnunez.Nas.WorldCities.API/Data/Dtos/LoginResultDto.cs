ca488aeb927a29fee24d1c19336f82869f4fbf63
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-22 12:00:52 -0700

    LoginResultDto

---
+++ WorldCities/Fnunez.Nas.WorldCities.API/Data/Dtos/LoginResultDto.cs
@@ -0,0 +1,17 @@
+namespace Fnunez.Nas.WorldCities.API.Data.Dtos;
+
+public class LoginResultDto
+{
+    /// <summary>
+    /// TRUE if the login attempt is successful, FALSE otherwise.
+    /// </summary>
+    public bool Success { get; set; }
+    /// <summary>
+    /// Login attempt result message
+    /// </summary>
+    public string Message { get; set; } = null!;
+    /// <summary>
+    /// The JWT token if the login attempt is successful, or NULL if not
+    /// </summary>
+    public string Token { get; set; }
+}
