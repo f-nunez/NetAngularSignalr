6230658dd25fa80e65695890c11edb07ebfcfdcf
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-22 13:06:21 -0700

    Migration to add Identity

--- WorldCities/Fnunez.Nas.WorldCities.API/Data/Migrations/20221022200325_Identity.Designer.cs
+++ WorldCities/Fnunez.Nas.WorldCities.API/Data/Migrations/20221022200325_Identity.Designer.cs
@@ -4,6 +4,7 @@ using Fnunez.Nas.WorldCities.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Fnunez.Nas.WorldCities.API.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221022200325_Identity")]
+    partial class Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
