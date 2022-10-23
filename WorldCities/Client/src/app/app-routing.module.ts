3ee10cab861c340c58980e5bcc277c2b2d9491c1
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-22 17:20:13 -0700

    Add AuthGuard to create and edit views

--- WorldCities/Client/src/app/app-routing.module.ts
+++ WorldCities/Client/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CitiesComponent } from './cities/cities.component';
 import { CityEditComponent } from './cities/city-edit/city-edit.component';
+import { AuthGuard } from './core/guards/auth.guard';
 import { CountriesComponent } from './countries/countries.component';
 import { CountryEditComponent } from './countries/country-edit/country-edit.component';
 import { HomeComponent } from './home/home.component';
@@ -10,11 +11,11 @@ import { LoginComponent } from './login/login.component';
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'cities', component: CitiesComponent },
-  { path: 'city', component: CityEditComponent },
-  { path: 'city/:id', component: CityEditComponent },
+  { path: 'city', component: CityEditComponent, canActivate: [AuthGuard] },
+  { path: 'city/:id', component: CityEditComponent, canActivate: [AuthGuard] },
   { path: 'countries', component: CountriesComponent },
-  { path: 'country', component: CountryEditComponent },
-  { path: 'country/:id', component: CountryEditComponent },
+  { path: 'country', component: CountryEditComponent, canActivate: [AuthGuard] },
+  { path: 'country/:id', component: CountryEditComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent }
 ];
 
