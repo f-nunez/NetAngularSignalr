902d1c3eb911ff2bb8268eb6efea633eaf6c8aa5
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-24 16:56:51 -0700

    Add endpoint to receive and response heartbeat on HealthCheck API

--- HealthCheck/Fnunez.Nas.HealthCheck.API/Program.cs
+++ HealthCheck/Fnunez.Nas.HealthCheck.API/Program.cs
@@ -23,6 +23,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+    options.AddPolicy(name: "AngularPolicy",
+        cfg =>
+        {
+            cfg.AllowAnyHeader();
+            cfg.AllowAnyMethod();
+            cfg.WithOrigins(builder.Configuration["AllowedCORS"]);
+        }));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -38,8 +47,12 @@ app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
+app.UseCors("AngularPolicy");
+
 app.UseHealthChecks(new PathString("/api/health"), new CustomHealthCheckOptions());
 
 app.MapControllers();
+
+app.MapMethods("/api/heartbeat", new[] { "HEAD" }, () => Results.Ok());
 
 app.Run();
