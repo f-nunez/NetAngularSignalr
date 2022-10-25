a1580ade83d7ffc2d79515784fb804f8deb75a40
Francisco Nuñez <fonfonferi@gmail.com>
2022-10-25 09:50:39 -0700

    Allow clients to send message

--- HealthCheck/Fnunez.Nas.HealthCheck.API/HealthCheckHub.cs
+++ HealthCheck/Fnunez.Nas.HealthCheck.API/HealthCheckHub.cs
@@ -3,4 +3,7 @@ using Microsoft.AspNetCore.SignalR;
 namespace Fnunez.Nas.HealthCheck.API;
 
 public class HealthCheckHub : Hub
-{ }
+{
+    public async Task ClientUpdate(string message) =>
+        await Clients.All.SendAsync("ClientUpdate", message);
+}
